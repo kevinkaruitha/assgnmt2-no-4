9f36352b4c2a1c66d6825d339f6dee4f701baf6c
kevinkaruitha <118059935+kevinkaruitha@users.noreply.github.com>
2022-11-14 09:28:49 +0300

    Add files via upload

---
+++ ASSGNTM(two)NO.4.java
@@ -0,0 +1,20 @@
+
+public class MyClass
+{
+    public static  int  regno; //static field
+    private static int phoneno; //static field
+  
+    private String  indexno;  //non-static field
+    Scanner input = new Scanner (System.in);
+    //ask user to enter phone number and his or her registration number
+   
+    public static int(intregno,Intphoneno)//constractor
+    {
+        regno =;
+        phoneno=;
+        System.out.println("enter registration number");
+        regno;sc.nextInt();
+        System.out.println("enter phone number number");
+        phoneno;sc.nextInt();
+        
+   
